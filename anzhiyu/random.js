fd74aecd48806622536b669814cc37ec3da48ae7
YE Xianzhi <xianzhi.ye@imt-atlantique.net>
2023-05-25 22:15:24 +0200

    Site updated: 2023-05-25 22:15:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/03/PyTorch-深度学习——nn-BCELoss/","2023/05/23/hello-world/","2023/05/07/学习设计-Hexo-主题（1）—— Prerequisites/","2023/05/08/学习设计-Hexo-主题（2）——layout/","2023/05/01/Git 学习笔记/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/01/Git 学习笔记/","2023/05/03/PyTorch-深度学习——nn-BCELoss/","2023/05/23/hello-world/","2023/05/07/学习设计-Hexo-主题（1）—— Prerequisites/","2023/05/08/学习设计-Hexo-主题（2）——layout/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
